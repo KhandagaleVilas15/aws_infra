f5640a27badd805860a9b8f505d80fbe0da6772d
Vilas <vkhandagale129@gmail.com>
2026-02-20 22:50:16 +0530

    fix file issue

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -79,7 +79,7 @@ resource "aws_autoscaling_group" "app" {
   target_group_arns   = [var.target_group_arn]
 
   health_check_type         = "ELB"
-  health_check_grace_period = 300 
+  health_check_grace_period = 420 
 
   launch_template {
     id      = aws_launch_template.app.id
